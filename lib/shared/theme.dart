14fa50af99fc80ef2df1ba93a22fec50347124a7
Aldi Jayadi <aldi.jayadi29@gmail.com>
2021-11-22 11:01:34 +0700

    initial commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 24.0;
+double defaultRadius = 17.0;
+
+Color kPrimaryColor = Color(0xff244489);
+Color kBlackColor = Color(0xff0D1931);
+Color kWhiteColor = Color(0xffFAFAFA);
+Color kRealWhiteColor = Color(0xffFFFFFF);
+Color kGreyColor = Color(0xffA1A1A1);
+Color kRedColor = Color(0xffD9435E);
+Color kGreenColor = Color(0xff1ABC9C);
+Color kBlueColor = Color(0xff119BFF);
+Color kInactiveColor = Color(0xff596C95);
+Color kTransparentColor = Colors.transparent;
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: kBlackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: kWhiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: kGreyColor);
+TextStyle blueTextStyle = GoogleFonts.poppins(color: kPrimaryColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight black = FontWeight.w900;
---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import '../../shared/theme.dart';
